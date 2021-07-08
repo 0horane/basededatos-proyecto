582ca0ef1e94fd0089dcd2835140074c49376f93
LeandroHorane <leandro.horane.siracusa@gmail.com>
2021-07-08 17:46:55 -0300

    actuallize coso php

--- 2021-7-7.php
+++ 2021-7-7.php
@@ -5,16 +5,24 @@ $sqlquery = "SELECT * FROM products";
 
 if ($omysqli->connect_errno) {printf("connect error: " . $omysqli->connect_error);}
 
-$result = $omysqli->query($sqlquery);
+if(!($result = $omysqli->query($sqlquery))){exit($omysqli->error);}
 
 while ($reg=mysqli_fetch_assoc($result)){
 	//for ($x=0;$x<count($reg);$x++){
-		printf(join($reg));
+		//printf(join($reg));
 		
 	//}
 	
-	printf("<br><br>");
+	//printf("<br><br>");
+	$products[]=$reg;
+	
 }
+$products[]="a";
+echo "<pre>";
+print_r($products);
+echo "</pre>";
+
+
 /*
 for ($x=0;x<$count($result);x++)
 	printf($result[x] . '\n')
