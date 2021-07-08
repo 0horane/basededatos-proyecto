aec9c99323c5bbc40012fba9858e979730040f60
LeandroHorane <leandro.horane.siracusa@gmail.com>
2021-07-08 17:11:01 -0300

    maso funciona

--- 2021-7-7.php
+++ 2021-7-7.php
@@ -7,7 +7,17 @@ if ($omysqli->connect_errno) {printf("connect error: " . $omysqli->connect_error
 
 $result = $omysqli->query($sqlquery);
 
+while ($reg=mysqli_fetch_assoc($result)){
+	//for ($x=0;$x<count($reg);$x++){
+		printf(join($reg));
+		
+	//}
+	
+	printf("<br><br>");
+}
+/*
 for ($x=0;x<$count($result);x++)
 	printf($result[x] . '\n')
+*/
 
->
+?>
