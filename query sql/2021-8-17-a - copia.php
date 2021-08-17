d30da491fbdbd59764efebe02726a443c8105561
LeandroHorane <leandro.horane.siracusa@gmail.com>
2021-08-17 13:00:45 -0300

    lo de hoy

--- query sql/2021-8-17-a - copia.php
+++ query sql/2021-8-17-a - copia.php
@@ -10,6 +10,8 @@ if ($omysqli->connect_errno) {printf("connect error: " . $omysqli->connect_error
 
 if(!($result = $omysqli->query($sqlquery))){exit($omysqli->error);}
 
+echo "<br><br><br><table border='1'>";
+
 while ($reg=mysqli_fetch_assoc($result)){
 	//for ($x=0;$x<count($reg);$x++){
 		//printf(join($reg));
@@ -17,39 +19,16 @@ while ($reg=mysqli_fetch_assoc($result)){
 	//}
 	
 	//printf("<br><br>");
-	$products[]=$reg;
-	
-}
-//$products[]="a";
-//echo "<pre>";
-//print_r($products);
-//echo "</pre>";
-
-echo "<br><br><br><table border='1'><tr>";
-$row=reset($products);
-$col=reset($row);
-echo "<th>" . $col . "</th>";
-while ($col=next($row)){
-	echo "<th>" . $col . "</th>";
-}
-echo "</tr>";
-while ($row=next($products)){
-	echo "<tr>";
-	$col=reset($row);
-	echo "<th>" . $col . "</th>";
-	while ($col=next($row)){
+	echo "</tr>";
+	while ($col=next($reg)){
 		echo "<th>" . $col . "</th>";
 	}
-	
-	
-	echo "</th>";
+	echo "</tr>";
 }
+
+
 
 echo "</table>"
 
-/*
-for ($x=0;x<$count($result);x++)
-	printf($result[x] . '\n')
-*/
 
 ?>
