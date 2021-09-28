33900809ee5e1cdd343f2763c15fb4e34b60d6b1
LeandroHorane <leandrohorane@gmail.com>
2021-09-28 20:09:28 -0300

    amended for mobile server

--- tatetiphp/r_sqlinit.php
+++ tatetiphp/r_sqlinit.php
@@ -1,8 +1,9 @@
 <?php
-define('serverIP', 'localhost');
+define('serverIP', '127.0.0.1'); //localhost
 define('user', 'root');
 define('database', 'uttt');
+define('password', 'root'); //''
 
-$mysqlinstance = new mysqli(serverIP, user, '', database);
+$mysqlinstance = new mysqli(serverIP, user, password, database);
 if ($mysqlinstance->connect_errno) {printf("connect error: " . $mysqlinstance->connect_error);}
 ?>
