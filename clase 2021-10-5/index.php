2e82a9f0f4b56691cb3bb4188da336c7dbdce331
LeandroHorane <leandrohorane@gmail.com>
2021-10-05 18:02:16 -0300

    lo de hoy

---
+++ clase 2021-10-5/index.php
@@ -0,0 +1,44 @@
+
+
+	<form>
+		ciudad:<input type='text' name='ciudad' value=<?php echo isset($_GET['ciudad']) ? $_GET['ciudad'] : '' ;?> > pais:<input type='text' name='pais' value=<?php echo isset($_GET['pais']) ?  $_GET['pais'] : '' ;?>> 
+		<input type='submit'>
+	</form>
+<?php
+
+if (!$link=mysqli_connect('localhost','root','','classicmodels')){die('error' . mysqli_error($link) );}
+
+if (!$result=mysqli_query($link,'SELECT * FROM offices WHERE city LIKE "%'.$_GET['ciudad']. '%" AND country LIKE "%'.$_GET['pais'].'%"')){die('error de coneccion' );}
+if (!$colarr=mysqli_query($link,'SHOW COLUMNS from offices')){die('error' . mysqli_error($link) );}
+
+?>
+<table border=1>
+<tr>
+<?php
+while ($row=mysqli_fetch_assoc($colarr)){
+	?>
+	
+			<th><?php echo $row['Field']; ?></th>
+<?php } ?> </tr> <?php
+reset($colarr);
+while ($row=mysqli_fetch_array($result)){
+//	print_r($row);
+	?>
+	<tr>
+	<?php
+		for ($i=0;$i<mysqli_num_fields($colarr);$i++){
+			?>
+			<td><?php echo $row[$i]; ?></td>
+			<?php
+		}
+	?>
+	</tr>
+<?php
+}
+
+
+
+
+
+
+?>
